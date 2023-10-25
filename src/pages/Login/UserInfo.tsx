67baed8ea6bb34cac43a57f375c1350fe5108ba4
furaha707 <zxcvyein950823@gmail.com>
2023-10-25 15:45:47 +0900

    Feat: 내정보 페이지 추가

---
+++ src/pages/Login/UserInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div>
+      내 정보
+    </div>
+  )
+}
+
+export default UserInfo
--- src/router/LoginRouter.tsx
+++ src/router/LoginRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Main from "../pages/Main"
 import UserJoin from "../pages/Login/UserJoin"
 import UserAccount from "../pages/Login/UserAccount"
+import UserInfo from "../pages/Login/UserInfo"
 
 const LoginRouter = () => {
   return (
@@ -9,6 +10,7 @@ const LoginRouter = () => {
         <Route path="/" element={<Main  />} />
         <Route path="/join" element={<UserJoin />} />
         <Route path="/account" element={<UserAccount />} />
+        <Route path="/mypage" element={<UserInfo />} />
       </Routes>
   )
 }
