2eb9f5b3a5749be9e3edc34359c483c2d8935b9d
furaha707 <zxcvyein950823@gmail.com>
2023-10-26 14:09:42 +0900

    Feat: 리스트페이지구현

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function App() {
           style={{
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'center',
           }}>
           <LoginRouter />
           <MainRouter />
