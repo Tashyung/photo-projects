f08b70d1bec691a7e437908e80b5e5a65e94e10d
Hojin <wkdghwls0623@gmail.com>
2023-11-04 00:29:56 +0900

    fix: receive img 오류해결

--- src/pages/Photo/PhotoList.tsx
+++ src/pages/Photo/PhotoList.tsx
@@ -1,22 +1,21 @@
-import styled from 'styled-components'
-import StyledButton from '../../styles/Button'
-import { useEffect, useState, useRef } from 'react'
-import { Flex } from '@chakra-ui/react'
-import { useImages } from '../../api/ImgFilter'
-import { useRecoilValue } from 'recoil'
+import styled from 'styled-components';
+import StyledButton from '../../styles/Button';
+import { useEffect, useState, useRef } from 'react';
+import { Flex } from '@chakra-ui/react';
+import { useImages } from '../../api/ImgFilter';
+import { useRecoilValue } from 'recoil';
 import { imgState, ImageData } from '../../store/ImgAtom';
-import { DateRange } from 'react-date-range'
-import './datepicker.css'
+import { DateRange } from 'react-date-range';
+import './datepicker.css';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-import PhotoItem from '../../components/template/PhotoItem'
+import PhotoItem from '../../components/template/PhotoItem';
 
 const PhotoList = () => {
-
   // 카테고리 상태관리
   const [active, setActive] = useState('');
   const [filter, setFilter] = useState('send');
-  
+
   const filterPhoto = (buttonType: string) => {
     setActive(buttonType);
     setFilter(buttonType);
@@ -26,14 +25,16 @@ const PhotoList = () => {
     } else if (buttonType === 'send') {
       setFilteredData(images?.sendImg);
     }
-  }
+  };
 
   // 리스트 전역상태 가져오기
   useImages();
   const images = useRecoilValue(imgState);
 
   // datePick 부분
-  const [filteredData, setFilteredData] = useState<ImageData[] | undefined>(images?.sendImg);
+  const [filteredData, setFilteredData] = useState<ImageData[] | undefined>(
+    images?.sendImg,
+  );
 
   // 이 부분의 부재였다 아무리 초기값을 filteredData 에 넣어줘도 렌더링이 안되는 이유,,
   useEffect(() => {
@@ -44,14 +45,17 @@ const PhotoList = () => {
   const [dateRange, setDateRange] = useState({
     startDate: new Date(),
     endDate: new Date(),
-    key: 'selection'
+    key: 'selection',
   });
 
-  const [ showDate, setShowDate ] = useState(false);
-  
+  const [showDate, setShowDate] = useState(false);
+
   // YY-MM-DD 형식으로 변환
   const startyear = dateRange.startDate.getFullYear();
-  const startmonth = String(dateRange.startDate.getMonth() + 1).padStart(2, '0');
+  const startmonth = String(dateRange.startDate.getMonth() + 1).padStart(
+    2,
+    '0',
+  );
   const startday = String(dateRange.startDate.getDate()).padStart(2, '0');
 
   const endyear = dateRange.endDate.getFullYear();
@@ -68,7 +72,7 @@ const PhotoList = () => {
   // 달력 토글
   const datePicking = () => {
     setShowDate(!showDate);
-  }
+  };
   const calendarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -76,15 +80,14 @@ const PhotoList = () => {
       if (calendarRef.current && !calendarRef.current.contains(event.target)) {
         setShowDate(false);
       }
-    }
+    };
 
     document.addEventListener('click', handleClickOutside);
-  
+
     return () => {
       document.removeEventListener('click', handleClickOutside);
-    }
+    };
   }, [calendarRef]);
-  
 
   const onRangeChange = (ranges: any) => {
     setDateRange({
@@ -92,59 +95,81 @@ const PhotoList = () => {
       endDate: ranges['selection'].endDate,
       key: ranges['selection'].key,
     });
-  }
+  };
 
   // 카테고리 필터링
   useEffect(() => {
-    const newData: ImageData[] | undefined = filter === 'received' ? images?.receiveImg : images?.sendImg;
+    const newData: ImageData[] | undefined =
+      filter === 'received' ? images?.receiveImg : images?.sendImg;
     const filteredData = newData?.filter((item: any) => {
       const timestamp = new Date(item.timestamp.toDate());
-      const itemDate = new Date(timestamp.getFullYear(), timestamp.getMonth(), timestamp.getDate());
+      const itemDate = new Date(
+        timestamp.getFullYear(),
+        timestamp.getMonth(),
+        timestamp.getDate(),
+      );
       return itemDate >= startDate && itemDate <= endDate;
     });
 
     if (filteredData) {
       setFilteredData(filteredData); // 필터링된 데이터를 업데이트합니다.
     }
-
   }, [startDate, endDate]);
-  
+
   return (
-    <>      
+    <>
       <Flex paddingTop={5} gap={5} justifyContent={'space-between'}>
         <Flex alignItems={'center'} gap={5} ref={calendarRef}>
-          <Flex onClick={() => datePicking()} alignItems={'center'} gap={5} style={{cursor: 'pointer'}}>
-            <p><img src="/calendar.png" alt="이미지" width="30" /></p>
-            <p>{formattedStartDate} ~ {formattedEndDate}</p> 
+          <Flex
+            onClick={() => datePicking()}
+            alignItems={'center'}
+            gap={5}
+            style={{ cursor: 'pointer' }}>
+            <p>
+              <img src="/calendar.png" alt="이미지" width="30" />
+            </p>
+            <p>
+              {formattedStartDate} ~ {formattedEndDate}
+            </p>
           </Flex>
-          {
-            showDate && 
+          {showDate && (
             <div>
-                <DateRange
+              <DateRange
                 editableDateInputs={true}
                 onChange={onRangeChange}
                 moveRangeOnFirstSelection={false}
                 ranges={[dateRange]}
                 showDateDisplay={false}
-                />     
-            </div>     
-          }
+              />
+            </div>
+          )}
         </Flex>
         <ButtonWrap>
-          <StyledButton onClick={() => filterPhoto('received')} rounded className={active === 'received' ? 'on' : ''} >received</StyledButton>
-          <StyledButton onClick={() => filterPhoto('send')} rounded oppositeColor className={active === 'send' ? 'on' : ''} >send</StyledButton>
+          <StyledButton
+            onClick={() => filterPhoto('received')}
+            rounded
+            className={active === 'received' ? 'on' : ''}>
+            received
+          </StyledButton>
+          <StyledButton
+            onClick={() => filterPhoto('send')}
+            rounded
+            oppositeColor
+            className={active === 'send' ? 'on' : ''}>
+            send
+          </StyledButton>
         </ButtonWrap>
       </Flex>
-      <ListWrap style={{marginTop: 20}}>
-        {filteredData?.map(item => (       
-           <PhotoItem key={item.imgId} imgId={item.imgId} imgURL={item.imgURL} />
+      <ListWrap style={{ marginTop: 20 }}>
+        {filteredData?.map((item) => (
+          <PhotoItem key={item.imgId} imgId={item.imgId} imgURL={item.imgURL} />
         ))}
       </ListWrap>
     </>
-  )
-}
+  );
+};
 
-export default PhotoList
+export default PhotoList;
 
 const ListWrap = styled.div`
   display: grid;
@@ -153,20 +178,20 @@ const ListWrap = styled.div`
   gap: 10px;
   overflow-y: auto;
   height: 81vh;
-`
+`;
 
 const ButtonWrap = styled.div`
-  display:flex;
+  display: flex;
   button {
     width: 135px; /* 오버라이딩할 스타일 속성 */
-    &:first-child{
+    &:first-child {
       margin-right: -15px;
     }
-    &:last-child{
+    &:last-child {
       margin-left: -15px;
     }
-    &.on{
-      z-index:10;
+    &.on {
+      z-index: 10;
     }
   }
-`
+`;
